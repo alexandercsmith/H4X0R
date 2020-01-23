7b4b06c10e9d9d6d26bf6b03c0c3fdadcf9db336
Alexander Smith <acsmith.dev@gmail.com>
2020-01-23 11:14:12 -0500

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by Alexander Smith on 1/23/20.
+//  Copyright © 2020 Alexander Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
